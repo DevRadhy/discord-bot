c0ea0f20665027c7364b75a526f97c605d2f99ca
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2022-10-16 20:32:53 -0300

    Add Command Init (#3)
    * Add commands method
    
    * Remove init commands from index

--- src/lib/Client.ts
+++ src/lib/Client.ts
@@ -1,7 +1,10 @@
 import { Client, Intents } from "discord.js";
+import * as functions from '../commands';
 
 export default {
   client: new Client({
     intents: [ Intents.FLAGS.GUILDS ]
-  })
+  }),
+
+  commands: Object.values(functions).map((command) => new command())
 };
