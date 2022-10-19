c4eacd87254ba61d84bd2543b3061fd90015a8e9
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2022-10-18 22:15:15 -0300

    Update mocks

--- src/mock/discord.ts
+++ src/mock/discord.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { Client, CommandInteraction, GuildMember, Role, User } from "discord.js";
+import { Client, CommandInteraction, Guild, GuildMember, Role, User } from "discord.js";
 import Discord from "../lib/Client";
 
 export class DiscordMock {
@@ -8,6 +8,7 @@ export class DiscordMock {
   public user: User;
   public member: GuildMember;
   public role: Role;
+  public guild: Guild;
 
   public interaction: CommandInteraction;
   
@@ -17,56 +18,75 @@ export class DiscordMock {
     this.RoleMock();
     this.UserMock();
     this.MemberMock();
+    this.GuildMock();
 
     this.InteractionMock();
   }
 
   private InteractionMock() {
-    this.interaction = Reflect.construct(CommandInteraction, [
-      this.client,
+    this.interaction = Object.assign(
+      Object.create(CommandInteraction),
       {
         data: [],
         id: BigInt(1),
         user: this.user,
-      },
-    ]);
-
-    this.interaction.reply = jest.fn();
-
-    this.interaction.options = {
-      ...this.interaction.options,
-      data: [],
-      getMember: jest.fn(),
-      getRole: jest.fn(),
-      getSubcommand: jest.fn(),
-    };
+        member: this.member,
+        guild: this.guild,
+        options: {
+          data: [],
+          getMember: jest.fn(),
+          getRole: jest.fn(),
+          getSubcommand: jest.fn(),
+        },
+        reply: jest.fn(),
+      }
+    );
   }
 
   private UserMock() {
-    this.user = Reflect.construct(User, [
-      this.client,
+    this.user = Object.assign(
+      Object.create(User),
       {
         id: "53908232506183680",
         username: "Mason",
         avatar: "a_d5efa99b3eeaa7dd43acca82f5692432",
         discriminator: "1337",
         bot: false,
-      },
-    ]);
+        avatarURL: jest.fn(),
+      });
   }
 
   private MemberMock() {
-    this.member = Reflect.construct(GuildMember, [
-      this.client,
+    this.member = Object.assign(
+      Object.create(GuildMember),
       {
+        id: "53908232506183680",
         user: this.user,
-        permissions: "2147483647",
-        roles: [],
-      }
-    ]);
+        roles: {
+          add: jest.fn(),
+          remove: jest.fn(),
+        }
+      });
   }
 
   private RoleMock() {
-    this.role = Reflect.construct(Role, []);
+    this.role = Object.assign(Object.create(Role), {});
+  }
+
+  private GuildMock() {
+    this.guild = Object.assign(
+      Object.create(Guild),
+      {
+        id: BigInt(1),
+        members: {
+          kick: jest.fn(),
+          ban: jest.fn(),
+        },
+        me: {
+          permissions: {
+            has: jest.fn(),
+          },
+        }
+      });
   }
 }
